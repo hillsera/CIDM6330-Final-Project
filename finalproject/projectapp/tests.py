238fe669a71cf2a1e565b13d2b8cb3923260df87
Sera Hill <sahill1@buffs.wtamu.edu>
2024-04-30 20:13:02 -0500

    updated projectapp tests.py

--- finalproject/projectapp/tests.py
+++ finalproject/projectapp/tests.py
@@ -8,27 +8,27 @@ from .models import LearningPath
 
 # Create your tests here.
 class LearningPathAPITest(APITestCase):
-    def test_add_learning_path(self):
-        # Prepare test data
-        learning_path_data = {
+    def setUp(self):
+        self.learning_path_data = {
             'id': 1,
             'title': 'Test Learning Path',
-            'duration': 300,  # Duration in seconds
+            'duration': 300,
             'progress': 0,
         }
 
-        # Send POST request to add a learning path
-        url = reverse('projectapp:learningpath-list')  # Assuming you have a URL named 'learning-path-list' for adding learning paths
-        response = self.client.post(url, data=learning_path_data, format='json')
+        self.list_url = reverse('projectapp:learningpath-list')
+
+    def test_add_learning_path(self):
+        response = self.client.post(self.list_url, data=self.learning_path_data, format='json')
 
         # Assert response status code
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Retrieve the learning path from the database
-        learning_path = LearningPath.objects.get(id=learning_path_data['id'])
+        learning_path = LearningPath.objects.get(id=self.learning_path_data['id'])
 
         # Assert that the learning path is added correctly
-        learning_path = LearningPath.objects.get(id=learning_path_data['id'])
-        self.assertEqual(learning_path.title, learning_path_data['title'])
-        self.assertEqual(learning_path.duration, learning_path_data['duration'])
-        self.assertEqual(learning_path.progress, learning_path_data['progress'])
+        learning_path = LearningPath.objects.get(id=self.learning_path_data['id'])
+        self.assertEqual(learning_path.title, self.learning_path_data['title'])
+        self.assertEqual(learning_path.duration, self.learning_path_data['duration'])
+        self.assertEqual(learning_path.progress, self.learning_path_data['progress'])
